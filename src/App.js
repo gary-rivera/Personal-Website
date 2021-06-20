ac56c3c9df9bcd720952a88fc244e2042195cf01
Gary Rivera <a.gary.rivera@gmail.com>
2021-06-19 17:36:27 -0700

    added styling for about section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import AboutMe from './pages/AboutMe'
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
     <div >
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- src/components/AboutTitle.js
+++ src/components/AboutTitle.js
@@ -1,28 +1,61 @@
 import home1 from "../img/home1.png";
+//Styling
+import styled from "styled-components";
 
 function AboutTitle() {
   return (
-    <div>
-      <div className="description">
+    <AboutStyle>
+      <DescriptionStyle>
         <div className="title"> 
-          <div className="hide">
+          <HideStyle>
             <h2> We work to make</h2>
-          </div>
-          <div className="hide">
+          </HideStyle>
+          <HideStyle>
             <h2> your <span> dreams </span>come</h2>
-          </div>
-          <div className="hide">
+          </HideStyle>
+          <HideStyle>
             <h2> true</h2>
-          </div>
+          </HideStyle>
         </div>
         <p> LINK to resume</p>
         <button> LINK </button>
-      </div>
-      <div className="image"> 
+      </DescriptionStyle>
+      <ImageStyle> 
         <img src={home1} alt="landing page profile pic"/>
-      </div>
-    </div>
+      </ImageStyle>
+    </AboutStyle>
   )
 }
+
+// Styled Component
+const AboutStyle = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const DescriptionStyle = styled.div`
+  flex: 1;
+  padding: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const ImageStyle = styled.div`
+  flex: 1;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const HideStyle = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutTitle;
